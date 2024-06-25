63c4476a0c38955e7c0d79b42c530383195f2203
Eduardo Hernandez <zohanshane@gmail.com>
2024-06-24 21:54:12 -0600

    24Junio2024

--- 101.SymmetricTree.js
+++ 101.SymmetricTree.js
@@ -5,3 +5,12 @@ Example 1:
 Input: root = [1,2,2,3,4,4,3]
 Output: true
 */
+var isSymmetric = function (root) {
+  function recurse(l, r) {
+    if (!l && !r) return true
+    if (!l || !r || l.val !== r.val) return false
+
+    return recurse(l.left, r.right) && recurse(l.right, r.left)
+  }
+  return recurse(root.left, root.right)
+}
