7d2680c481b58369c2ec5a178e793dcca2e5c24b
Eduardo Hernandez <zohanshane@gmail.com>
2024-07-06 22:01:12 -0600

    06Julio2024

--- 1518.WaterBottles.js
+++ 1518.WaterBottles.js
@@ -5,4 +5,22 @@ The operation of drinking a full water bottle turns it into an empty bottle.
 
 Given the two integers numBottles and numExchange, return the maximum number of water bottles you can drink.
 */
-var numWaterBottles = function (numBottles, numExchange) {}
+var numWaterBottles = function (numBottles, numExchange) {
+  let check = 0
+  let result = 0
+  while (numBottles > 0) {
+    numBottles--
+    result += 1
+    check += 1
+    if (check == numExchange) {
+      numBottles++
+      check = 0
+    }
+
+    if (numBottles == 0) break
+    console.log(check)
+    console.log(`botles: ${numBottles}`)
+  }
+  return result
+}
+console.log(numWaterBottles(15, 4))
