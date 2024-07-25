3037858e34286217f10739888bd59b7f1581f2a6
Eduardo Hernandez <zohanshane@gmail.com>
2024-07-24 22:37:25 -0600

    24Julio2024

---
+++ 520.DetectCapital.js
@@ -0,0 +1,29 @@
+/*
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+*/
+var detectCapitalUse = function (word) {
+  if (word === word.toUpperCase()) {
+    return true
+  }
+  if (word === word.toLowerCase()) {
+    return true
+  }
+
+  let letters = Array.from(word)
+  if (letters[0] == letters[0].toUpperCase()) {
+    for (let i = 1; i < letters.length; i++) {
+      console.log(letters[i])
+      if (letters[i] !== letters[i].toLowerCase()) {
+        return false
+      }
+    }
+    return true
+  }
+}
+let word = 'FlaG'
+console.log(detectCapitalUse(word))
